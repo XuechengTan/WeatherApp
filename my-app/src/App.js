987e553b42e4e37f34a67e410181f2f0bebb5e03
zf49 <zwan686@aucklanduni.ac.nz>
2021-04-27 19:16:22 +0800

    Map: city

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
 
   let map = new BMap.Map("allmap");//initialize Map
 
-  let point = new BMap.Point(116.331398,39.897445);//SET initial center point
+  let point = new BMap.Point(10,10);//SET initial center point
   map.centerAndZoom(point,12);
   
   function myFun(result){
