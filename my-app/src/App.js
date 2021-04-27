38a2b2f48554d1e18bd50f47deb3d739c25319a1
zf49 <zwan686@aucklanduni.ac.nz>
2021-04-27 19:11:02 +0800

    Map+ city

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,30 @@
 
+
+import BMap  from 'BMap';
+
+
 function App() {
+
+  let map = new BMap.Map("allmap");//initialize Map
+
+  let point = new BMap.Point(116.331398,39.897445);//SET initial center point
+  map.centerAndZoom(point,12);
+  
+  function myFun(result){
+    var cityName = result.name;
+    map.setCenter(cityName);
+    console.log("current city:"+cityName);
+  }
+  var myCity = new BMap.LocalCity();
+  myCity.get(myFun); 
+
+
   return (
     <div className="App">
      <h1>GROUP 29</h1>
-    </div>
+  </div>
   );
 }
 
 export default App;
+
